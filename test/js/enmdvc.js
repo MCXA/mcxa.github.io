8245b3b71036c0e5583374aff1cd441f29a181ab
Egor Rumyantsev <RumyantsevGT@ya.ru>
2020-05-09 01:51:09 +0300

    Update enmdvc.js

--- test/js/enmdvc.js
+++ test/js/enmdvc.js
@@ -64,3 +64,16 @@ function handleError(error) {
   console.error('Error: ', error);
 }
 
+
+// Перечисление в цикле камер и микрофонов
+
+navigator.mediaDevices.enumerateDevices()
+.then(function(devices) {
+  devices.forEach(function(device) {
+    console.log(device.kind + ": " + device.label +
+                " id = " + device.deviceId);
+  });
+})
+.catch(function(err) {
+  console.log(err.name + ": " + err.message);
+});
